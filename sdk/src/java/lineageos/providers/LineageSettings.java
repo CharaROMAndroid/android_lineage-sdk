f0155e158b515ed98ddb9c1791d2980d907181ca
Oliver Scott <olivercscott@gmail.com>
2024-04-30 14:13:39 -0400

    User timeout feature II
    Add new mode where the timeout stops the user
    
    Issue: calyxos#436
    Change-Id: Icf1c52be26af980f1afd18387e01f554f973bf19

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -3120,6 +3120,16 @@ public final class LineageSettings {
                 sNonNegativeLongValidator;
 
         /**
+         * Whether to disable the user when it is scheduled to deactivate
+         * @hide
+         */
+        public static final String USER_ACTIVITY_END_TIME_MODE = "user_activity_end_time_mode";
+
+        /** @hide */
+        public static final Validator USER_ACTIVITY_END_TIME_MODE_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 1);
+
+        /**
          * Used to enable / disable network access for new installed apps
          *
          * Values are:
@@ -3188,6 +3198,7 @@ public final class LineageSettings {
             VALIDATORS.put(TRUST_WARNINGS, TRUST_WARNINGS_VALIDATOR);
             VALIDATORS.put(VOLUME_PANEL_ON_LEFT, VOLUME_PANEL_ON_LEFT_VALIDATOR);
             VALIDATORS.put(USER_ACTIVITY_END_TIME, USER_ACTIVITY_END_TIME_VALIDATOR);
+            VALIDATORS.put(USER_ACTIVITY_END_TIME_MODE, USER_ACTIVITY_END_TIME_MODE_VALIDATOR);
             VALIDATORS.put(DEFAULT_RESTRICT_NETWORK_DATA, DEFAULT_RESTRICT_NETWORK_DATA_VALIDATOR);
             VALIDATORS.put(USER_SWITCHER_HIDDEN_WHEN_LOCKED,
                     USER_SWITCHER_HIDDEN_WHEN_LOCKED_VALIDATOR);
